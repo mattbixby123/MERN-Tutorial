22cbd01b4709684c7c7a242d2a0b32135de147b7
mattbixby123 <matthew.bixby1@gmail.com>
2024-08-13 15:03:06 -0400

    saving for full restart of computer

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='animate-ping 2-16 h-16 m-8 rouded-full bg-sky-600'>Spinner</div>
+  )
+}
+
+export default Spinner
