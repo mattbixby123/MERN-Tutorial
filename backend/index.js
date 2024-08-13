e47f99f4f9e19bd4c1a1e58215b974772ded47b3
mattbixby123 <matthew.bixby1@gmail.com>
2024-08-13 14:04:09 -0400

    simple frontend initialized and cors policy middleware added to backend server index.js file

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ const app = express();
 // Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Defauly of cors(*)
+app.use(cors())
+
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Welcome to MERN Stack Tutorial')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    // Fetch data from the Express server
+    axios.get('http://localhost:5555/books')
+      .then(response => console.log(response))
+      // .then(response => setBooks(response.data))
+      .catch(error => console.error(error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MERN Stack Book App</h1>
     </div>
   );
-}
-
+};
 export default App;
